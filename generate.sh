092f1262d0eb9c7cb47893ec4fe75875baa47570
yikezawa <y.ikezawa93@gmail.com>
2020-03-21 17:41:56 +0900

    mod

--- generate.sh
+++ generate.sh
@@ -1,2 +1,1 @@
-protoc -I/usr/local/include -I. -I$GOPATH/src  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --grpc-gateway_out=logtostderr=true:. src/authpb/auth.proto
-protoc -I/usr/local/include -I. -I$GOPATH/src  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. src/authpb/auth.proto
+protoc -I/usr/local/include -I. -I$GOPATH/src  -I$GOPATH/src/github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis --go_out=plugins=grpc:. src/interfaces/rpc/pictweetpb/pictweet.proto
